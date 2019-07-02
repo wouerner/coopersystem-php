5e61b67efb78c91e17a7cce383f4cc0b05c151a3
wouerner <wouerner@protonmail.com>
2019-07-02 18:36:59 -0300

    [ADD] ajsutes nos retorno das controllers

--- tests/Feature/PedidoTest.php
+++ tests/Feature/PedidoTest.php
@@ -8,27 +8,42 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PedidoTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-
-    public function testBasicExample()
+    public function testStorePedido201()
     {
         $response = $this->withHeaders(['Content-Type' => 'application/x-www-form-urlencoded'])
-            ->post('/api/pedido', ['produtos' =>[1 => 1] ]);
+            ->post('/api/pedido', [
+                'solicitante' => 'teste1',
+                'endereco' => 'endereço1',
+                'despachante' => 'despachante1',
+                'situacao' => 'd',
+                'produtos' => [1 => 1]
+            ]
+        );
 
-        $response
-            ->assertStatus(201)
+        $response->assertStatus(201)
             ->assertJson([
                 'message' => 'Sucesso!',
-            ]);
+            ])
+            ->assertJsonStructure(['message']);
+    }
+
+    public function testUpdatePedido200()
+    {
+        $response = $this->withHeaders(['Content-Type' => 'application/x-www-form-urlencoded'])
+            ->put('/api/pedido/1', [
+                'solicitante' => 'teste2',
+                'endereco' => 'endereço2',
+                'despachante' => 'despachante12',
+                'situacao' => 'd',
+                'produtos' => [1 => 2]
+            ]
+        );
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => 'Sucesso!',
+            ])
+            ->assertJsonStructure(['message']);
     }
 }
